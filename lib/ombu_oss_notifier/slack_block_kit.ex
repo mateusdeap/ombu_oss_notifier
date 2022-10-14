2ce16ae7b2524f660566d62538c4cce89653eee1
Mateus Pereira <mateusdeap@gmail.com>
2022-10-14 10:35:31 -0300

    Adds module with functions that create slack block kit elements

--- lib/ombu_oss_notifier/slack_block_kit.ex
+++ lib/ombu_oss_notifier/slack_block_kit.ex
@@ -1,8 +1,12 @@
 defmodule OmbuOssNotifier.SlackBlockKit do
   
-  def blocks(content \\ []) do
+  # I honestly don't know a better way to do this since
+  # in some cases we may be piping the results of iterators
+  # into blocks
+  def blocks(content) when is_list(content) do
     %{"blocks" => content}
   end
+  def blocks(content), do: %{"blocks" => [content]}
 
   def link_button(link_text, button_text, value, url) do
     %{
@@ -21,6 +25,16 @@ defmodule OmbuOssNotifier.SlackBlockKit do
         "value" => value,
         "url" => url,
         "action_id" => "button-action"
+      }
+    }
+  end
+
+  def section(text) do
+    %{
+      "type" => "section",
+      "text" => %{
+        "type" => "mrkdwn",
+        "text" => text
       }
     }
   end
