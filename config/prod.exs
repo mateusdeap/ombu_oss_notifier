ff72c651749d19e0e009de6f96d3aac7c7ab2a43
Mateus Pereira <mateusdeap@gmail.com>
2022-11-18 13:48:03 -0300

    Removes `cache_static_manifest` option and sets logging to debug to help figure out issue in heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,10 @@ config :ombu_oss_notifier, OmbuOssNotifierWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "ombu-oss-notifier.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
