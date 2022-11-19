830be0a1a5afb973b89b3dc4860b8b62d0b01c05
Mateus Pereira <mateusdeap@gmail.com>
2022-11-18 22:15:17 -0300

    Adds https key

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ import Config
 config :ombu_oss_notifier, OmbuOssNotifierWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "ombu-oss-notifier.herokuapp.com", port: System.get_env("PORT")],
+  https: [port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
