48bf8750f6725d4c67b0ebf307ca65bfdf2e9d93
Mateus Pereira <mateusdeap@gmail.com>
2023-01-20 09:11:21 -0300

    Apply linter

--- lib/ombu_oss_notifier/issues_notifications_job.ex
+++ lib/ombu_oss_notifier/issues_notifications_job.ex
@@ -16,21 +16,26 @@ defmodule OmbuOssNotifier.IssuesNotificationsJob do
 
   def handle_info(:work, state) do
     cond do
-      is_saturday() -> run_in(48, :hour)
-      is_sunday() -> run_in(24, :hour)
+      is_saturday() ->
+        run_in(48, :hour)
+
+      is_sunday() ->
+        run_in(24, :hour)
+
       is_weekday() ->
         Fastruby.list_issues()
         |> SlackNotifier.notify()
-        
+
         run_in(24, :hour)
     end
-    
+
     {:noreply, state}
   end
 
   def current_weekday do
-    today = Date.utc_today()
-            |> Date.day_of_week()
+    today =
+      Date.utc_today()
+      |> Date.day_of_week()
 
     case today do
       1 -> :monday
@@ -59,28 +64,33 @@ defmodule OmbuOssNotifier.IssuesNotificationsJob do
   end
 
   def run_in(time, :second) do
-    Process.send_after(self(), :work, time*1000)
+    Process.send_after(self(), :work, time * 1000)
   end
+
   def run_in(time, :minute) do
-    Process.send_after(self(), :work, time*60*1000)
+    Process.send_after(self(), :work, time * 60 * 1000)
   end
+
   def run_in(time, :hour) do
-    Process.send_after(self(), :work, time*60*60*1000)
+    Process.send_after(self(), :work, time * 60 * 60 * 1000)
   end
-  
+
   defp schedule(time) do
-    time_diff_seconds = DateTime.utc_now()
-    |> calculate_time_diff(time)
+    time_diff_seconds =
+      DateTime.utc_now()
+      |> calculate_time_diff(time)
 
     run_in(time_diff_seconds, :second)
   end
 
   def calculate_time_diff(today, time) when today.hour >= time.hour do
     tomorrow = DateTime.add(today, 1, :day)
+
     Date.new!(tomorrow.year, tomorrow.month, tomorrow.day)
     |> DateTime.new!(time)
     |> DateTime.diff(today)
   end
+
   def calculate_time_diff(today, time) do
     Date.new!(today.year, today.month, today.day)
     |> DateTime.new!(time)
